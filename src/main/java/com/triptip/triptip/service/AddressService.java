b8c76a3a805e882bdc9d8805adac419f1e179cd9
ReQss <ReQsPL@gmail.com>
2024-04-17 23:55:00 +0200

    small fix

--- src/main/java/com/triptip/triptip/service/AddressService.java
+++ src/main/java/com/triptip/triptip/service/AddressService.java
@@ -5,7 +5,7 @@ import com.triptip.triptip.model.User;
 import com.triptip.triptip.repository.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+//big change
 @Service
 public class AddressService {
     @Autowired
