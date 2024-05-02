11b25beff83c40d3656f011d6f179ae89bb90703
ReQss <ReQsPL@gmail.com>
2024-05-02 21:16:17 +0200

    payment init

--- src/main/java/com/triptip/triptip/controller/OrderController.java
+++ src/main/java/com/triptip/triptip/controller/OrderController.java
@@ -6,18 +6,16 @@ import com.triptip.triptip.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin({"http://localhost:3000", "http://localhost:4000"})
 public class OrderController {
     @Autowired
     OrderService orderService;
-    @PostMapping("/addOrder")
-    public ResponseEntity<String> addOrder(@RequestParam String login) {
+
+    @GetMapping ("/addOrder") // Change to GET mapping
+    public ResponseEntity<String> addOrder(@RequestParam("login") String login) {
         try {
             orderService.addOrder(login);
             return new ResponseEntity<>("Order added successfully", HttpStatus.OK);
@@ -25,4 +23,5 @@ public class OrderController {
             return new ResponseEntity<>("Failed to add order: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
 }
---
+++ src/main/java/com/triptip/triptip/service/PaymentService.java
@@ -0,0 +1,9 @@
+package com.triptip.triptip.service;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public interface PaymentService  {
+//    @Value("${stripe.api.key}")
+//    public String stripeSecretKey;
+
+}
