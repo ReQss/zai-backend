21e833dc39ffcb625b0acadb519ccc34ca9c4888
ReQss <ReQsPL@gmail.com>
2024-05-21 22:36:38 +0200

    spring security, hash for password

--- src/main/java/com/triptip/triptip/controller/UserController.java
+++ src/main/java/com/triptip/triptip/controller/UserController.java
@@ -7,6 +7,7 @@ import com.triptip.triptip.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -20,6 +21,35 @@ public class UserController {
     private UserService userService;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/addUser")
+    public User addUser(@RequestBody User user) {
+
+        String encodedPassword = this.passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        System.out.print((encodedPassword));
+        return userService.addUser(user);
+
+    }
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+        String token = "token -" + user.getLogin();
+        String encodedPassword = this.passwordEncoder.encode(user.getPassword());
+
+        User storedUser = userRepository.findByLogin(user.getLogin()); // Assuming you have a method like this in your repository
+
+        if (storedUser == null || !passwordEncoder.matches(user.getPassword(), storedUser.getPassword())) {
+            return ResponseEntity.badRequest().body("Invalid login credentials");
+        }
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("token", token);
+        response.put("user", storedUser);
+        return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+    }
+
     @CrossOrigin ("http://localhost:3000")
     @GetMapping("/getUser/{id}")
     public ResponseEntity<User> getUser(@PathVariable int id) {
@@ -42,26 +72,13 @@ public class UserController {
         else
             userRepository.delete(user);
     }
-    @PostMapping("/addUser")
-    public User addUser(@RequestBody User user) {
-        return userService.addUser(user);
 
-    }
     @PutMapping("/editUser/{id}")
     public ResponseEntity<User> editUser(@PathVariable int id,@RequestBody User user){
         userService.editDetails(id,user.getFname(),user.getLname(),user.getEmail(), user.getPhone());
         return ResponseEntity.ok().build();
     }
-    @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody User user) {
-        String token = "token -" + user.getLogin();
-        User loggedUser = userService.login(user.getLogin(), user.getPassword());
-        if (loggedUser == null) return  ResponseEntity.badRequest().body("Invalid login credentials");
-        Map<String, Object> response = new HashMap<>();
-        response.put("token", token);
-        response.put("user", loggedUser); // Use loggedUser instead of user
-        return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
-    }
+
     @PutMapping("/editUserAddress/{userId}")
     public ResponseEntity<User> editAddress(@PathVariable int userId, @RequestBody Address address) {
         userService.editUserAddress2(userId, address);
